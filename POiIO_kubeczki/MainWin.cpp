68373bd086f4a89e9e90e0032a1c28394501b45a
jakubkiraga <jkiraga@student.agh.edu.pl>
2026-04-13 16:53:40 +0200

    Wykonanie zadania 1 - utworzenie okna aplikacji

---
+++ POiIO_kubeczki/MainWin.cpp
@@ -0,0 +1,15 @@
+#include "MainWin.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	POiIOkubeczki::MainWin form;
+	Application::Run(% form);
+	return 0;
+}
